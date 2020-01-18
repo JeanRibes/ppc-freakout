468827ab25f85add8683466dcc9ff59f85c05d84
JeanRibes <jean.ribes@bde-insa-lyon.fr>
2020-01-18 22:19:53 +0100

    broadcast local

--- matchmaking.py
+++ matchmaking.py
@@ -1,124 +1,28 @@
 import socket, struct
 from threading import Thread
 import typing
+import time
 
 from array import array
 
-udp_port = 1352
+udp_announce_port = 5688
 server_ip = "vps.ribes.ovh"
 
-
-def ipv4_from_ints(ints):
-    return ".".join(str(i) for i in ints)
-
-
-def socket_from_ints(ints):
-    return (
-        ipv4_from_ints(ints[:4]),
-        ints[4]
-    )
-
-
-def list_servers():
-    mms: socket.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
-    mms.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    mms.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-
-    mms.sendto(b'list', (server_ip, udp_port))
-    buf = mms.recv(1024)
-    mms.close()
-
-    # print(len(buf))
-    data = list(struct.iter_unpack('5H', buf))
-    servers = []
-    for intsock in data:
-        servers.append(socket_from_ints(intsock))
-    return servers
-
-
-def signal_matchmaking(state: bytes, local_port):
-    mms: socket.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
-    mms.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    mms.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    mms.bind(("0.0.0.0", local_port))
-    mms.sendto(state, (server_ip, udp_port))
-    mms.close()
-
-
-def ipv4_to_int(ip: str):
-    return [int(x) for x in ip.split('.')]
-
-
-def socket_to_bytes(ip, port) -> bytes:
-    # array.array('H',[*ipv4_to_int(ip),port]).tobytes()
-    return struct.pack("!4BH", *ipv4_to_int(ip), port)  # 4B : l'adresse IP (signed char) H: le port
-
-
-def list_to_bytes(addr_list):
-    blist = []
-    for ip, port in addr_list:
-        blist.extend(ipv4_to_int(ip))
-        blist.append(port)
-    buf = array('H', blist)
-    return buf
-
-
-def metadata_to_bytes(metadata) -> bytes:
-    length, usernames = metadata
-    s = str(length) + ";,;" + ";,;".join(usernames)
-    return s.encode('utf-8')
-
-
-class MetadataServer(Thread):
-    sock: socket.socket = None
-    game_started = False
-    # metadata: typing.Tuple[int, typing.List[str]]
-    number_clients = 0
-    clients_list: typing.List[str]
-
-    def __init__(self, port, game_started, clients_list, number_clients=3):
-        self.sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-        self.port = port
-        self.game_started = game_started
-        self.clients_list = ['maurincomme', 'Zalgo']  # clients_list
-        self.number_clients = number_clients
-        super().__init__(daemon=True)
-
+class BroadcastGame(Thread):
+    message = b'0 joueurs'
+    run=True
     def run(self) -> None:
-        print("starting metadata server")
-        self.sock.bind(('', self.port))
-        while not self.game_started:
-            request, client = self.sock.recvfrom(255)
-            #print(request, str(client))
-            if request == b'can i haz metadata ?':
-                self.sock.sendto(
-                    metadata_to_bytes(
-                        (self.number_clients, self.clients_list)
-                    ), client)
-            else: # le serveur de matchmaking demande si on attent toujours
-                self.sock.sendto(b'im alive', client)
+        sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # pour autoriser le boarast
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)  # pour éviter d'avoir l'erreur
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # de permission quand on ferme mal le jeu
+        while self.run:
+            sock.sendto(self.message, ('255.255.255.255', udp_announce_port))
+            time.sleep(3)
 
-
-def bytes_to_metadata(binstr: bytes) -> typing.Tuple[int, typing.List[str]]:
-    data = binstr.decode('utf-8').split(';,;')
-    count = int(data[0])
-    usernames = data[1:]
-    return count, usernames
-
-
-def get_metadata(host: typing.Tuple[str, int]) -> typing.Tuple[int, typing.List[str]]:
-    mms: socket.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
-    mms.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    mms.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    mms.sendto(b'can i haz metadata ?', host)
-    mms.settimeout(1)
-    res = mms.recv(1024)
-    mms.close()
-    return bytes_to_metadata(res)
+    def setmessage(self, msg:str):
+        self.message = msg.encode('utf-8')
 
 
 if __name__ == '__main__':
-    testmetadata = (2, ['Eric Maurincomme', 'ᛏᚺᛟᚱ ᛗᛃᛟᛚᚾᛇᚱ', "̘̰Z̭̙͈̺A̜̞̻͈̤͜L̞͇̙͍̜̦̠G͈̫̫͟O̲̰͡!̛̼̲̪̠̙̰̠"])
-    assert bytes_to_metadata(metadata_to_bytes(testmetadata)) == testmetadata
+    BroadcastGame().start()
--- matchmakingclient.py
+++ matchmakingclient.py
@@ -1,72 +1,17 @@
-import socket, struct
-import sys
-
-from array import array
-
-from data import ClientMessage, TYPE_JOIN
-from kbhit import KBHit
-
-from matchmaking import list_servers, get_metadata
-
-
-def server_chooser():
-    print("Connexion au serveur maître...")
-    servers = list_servers()
-
-    if len(servers) == 0:
-        return None
-
-    print("Liste des serveurs :")
-    for s in servers:
-        print("[ ] " + str(s), end=" ", flush=True)
-        try:
-            metadata = get_metadata(s)
-            print("{} connectés: {}".format(metadata[0], ", ".join(metadata[1])))
-        except socket.timeout:
-            print("Serveur injoignable")
-
-    print("Faites votre choix avec les flèches haut-bas, validez avec droite")
-    selected = 0
-    kb = KBHit()
-
-    def highlight(selected):
-        n = 1 + len(servers) - selected
-        print("\033[{}A\r[*]\033[{}B\r".format(str(n), str(n)), end='', flush=True)
-
-    def revert(n):
-        n = 1 + len(servers) - selected
-        print("\033[{}A\r[ ]\033[{}B\r".format(str(n), str(n)), end='', flush=True)
-
-    highlight(selected)
-    while True:
-        kc = kb.getarrow()
-        revert(selected)
-        # print('\r  ', end='',flush=True)
-        if kc == 0:
-            selected = (selected - 1) % len(servers)
-        elif kc == 2:
-            selected = (selected + 1) % len(servers)
-        highlight(selected)
-        if kc in [1, None]:
-            break
-    kb.set_normal_term()
-    print("vous avez choisi ", end="")
-    return servers[selected]
-
+import socket
+from matchmaking import udp_announce_port
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        ip = sys.argv[1]
-        port = sys.argv[2]
-        host = (ip, int(port))
-    else:
-        host = server_chooser()
-        print()
-        if host is None:
-            host=(input('ip: '),int(input('port: ')))
-    username=input("Choisissez un nom d'utilisateur: ")
+    sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM, proto=socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)  # pour autoriser le boarast
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)  # pour éviter d'avoir l'erreur
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # de permission quand on ferme mal le jeu
 
-    gamesock = socket.socket()
-    gamesock.connect(host)
+    sock.bind(('',udp_announce_port)) # on veut tout recevoir
 
-    gamesock.send(ClientMessage(type_message=TYPE_JOIN, payload=username).serialize())
+    sock.settimeout(5)
+    try:
+        msg, addr = sock.recvfrom(4096)
+        print(f"{msg} reçu de {addr}")
+    except socket.timeout:
+        pass
